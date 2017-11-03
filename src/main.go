9576574ed1b9cb0f5c7505b51e0b274cc1e866cf
Your Name <you@example.com>
2017-11-03 15:55:26 +0800

    'update'

--- src/main.go
+++ src/main.go
@@ -20,19 +20,26 @@ func usage() {
 
 func main() {
 	var err error
+	//判断参数个数少于2个则提示
 	if len(os.Args) < 2 {
+		//提示
 		usage()
 		os.Exit(1)
 	}
 
+	//获取当前目录
 	wd,err := os.Getwd()
 	fmt.Println("telegram_go:",wd + "/.telegram_go")
 
+	//命令map
 	commands := map[string]int{"auth": 1, "msg": 2, "list": 0}
+	//验证标识
 	valid := false
+	//循环命令map，对比看一下执行的是哪个命令
 	for k, v := range commands {
 		if os.Args[1] == k {
 			if len(os.Args) < v+2 {
+				//参数错误则弹出提示以后退出
 				usage()
 				os.Exit(1)
 			}
@@ -41,18 +48,21 @@ func main() {
 		}
 	}
 
+	//验证不通过则弹出用例然后退出
 	if !valid {
 		usage()
 		os.Exit(1)
 	}
 
 	//m, err := mtprotox.NewMTProto(os.Getenv("HOME") + "/.telegram_go")
+	//获取 结构体：NewMTProto
 	m, err := mtprotox.NewMTProto(wd + "/.telegram_go")
 	if err != nil {
 		fmt.Printf("Create failed: %s\n", err)
 		os.Exit(2)
 	}
 
+	//连接
 	err = m.Connect()
 	if err != nil {
 		fmt.Printf("Connect failed: %s\n", err)
